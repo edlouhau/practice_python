87c8974bd4e762e7e1e5fc1865f238f2f75d574f
Eduardo Louhau <louhaudev@gmail.com>
2023-03-23 22:01:39 -0400

    Exercise 5.

---
+++ ex_5_list_overlap.py
@@ -0,0 +1,19 @@
+# Take two lists, say for example these two:
+#   a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#   b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+# and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure your program works on two lists of different sizes.
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+
+# Convert lists to sets and find the intersection
+common_elements = set(a).intersection(set(b))
+
+# Convert the result back to a list
+result = list(common_elements)
+
+# Print the result
+print(result)
+
+
